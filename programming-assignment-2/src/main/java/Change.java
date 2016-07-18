b5715582099340077557a15c39437c8c4a4e01c4
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2016-07-18 10:54:41 +0300

    solved programming-assignment-2 - first task

--- programming-assignment-2/src/main/java/Change.java
+++ programming-assignment-2/src/main/java/Change.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 
 public class Change {
-    private static int getChange(int m) {
-        //write your code here
-        return m;
+    private final static int COIN_DENOMINATION_10 = 10;
+    private final static int COIN_DENOMINATION_5 = 5;
+    private final static int COIN_DENOMINATION_1 = 1;
+    private final static int[] COIN_DENOMINATIONS = new int[]{COIN_DENOMINATION_10, COIN_DENOMINATION_5, COIN_DENOMINATION_1};
+
+    public static int getChange(int m) {
+        int coinsAmount = 0;
+        for (int coinDenomination : COIN_DENOMINATIONS) {
+            while (m >= coinDenomination) {
+                m -= coinDenomination;
+                coinsAmount++;
+            }
+        }
+        return coinsAmount;
     }
 
     public static void main(String[] args) {
--- programming-assignment-2/src/test/groovy/ChangeTest.groovy
+++ programming-assignment-2/src/test/groovy/ChangeTest.groovy
@@ -1,10 +1,23 @@
 import org.junit.Rule
 import org.junit.rules.Stopwatch
 import spock.lang.Specification
-
+import spock.lang.Unroll
 
 
 class ChangeTest extends Specification {
     @Rule
     public Stopwatch stopwatch = new MeasuringRule()
+
+    @Unroll
+    def "getChange for #m should return #expectedResult"() {
+        expect:
+        Change.getChange(m) == expectedResult
+        where:
+        m  || expectedResult
+        2  || 2
+        6  || 2
+        11 || 2
+        14 || 5
+        28 || 6
+    }
 }
