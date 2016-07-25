e7623ad370c4e6d65922e6873cc3683173ba9241
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2016-07-25 22:06:33 +0300

    solved programming-assignment-2 - DifferentSummands task

--- programming-assignment-2/src/main/java/DifferentSummands.java
+++ programming-assignment-2/src/main/java/DifferentSummands.java
@@ -1,12 +1,23 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class DifferentSummands {
-    private static List<Integer> optimalSummands(int n) {
-        List<Integer> summands = new ArrayList<Integer>();
+    public static List<Integer> optimalSummands(int n) {
+        List<Integer> summands = new ArrayList<>();
+        int total = 0;
+        int i = 1;
+        while (total + i <= n) {
+            total += i;
+            summands.add(i);
+            i++;
+        }
+        int lastIndex = summands.size() - 1;
+        summands.set(lastIndex, summands.get(lastIndex) + n - total);
         //write your code here
         return summands;
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
--- programming-assignment-2/src/test/groovy/DifferentSummandsTest.groovy
+++ programming-assignment-2/src/test/groovy/DifferentSummandsTest.groovy
@@ -1,10 +1,22 @@
 import org.junit.Rule
 import org.junit.rules.Stopwatch
 import spock.lang.Specification
-
+import spock.lang.Unroll
 
 
 class DifferentSummandsTest extends Specification {
     @Rule
     public Stopwatch stopwatch = new MeasuringRule()
+
+    @Unroll
+    def "optimalSummands of #n should be #expectedResult"() {
+        expect:
+        DifferentSummands.optimalSummands(n) == expectedResult
+        where:
+        n || expectedResult
+        2 || [2]
+        6 || [1, 2, 3]
+        8 || [1, 2, 5]
+        3 || [1, 2]
+    }
 }
