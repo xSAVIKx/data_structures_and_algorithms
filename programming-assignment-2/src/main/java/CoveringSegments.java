c462866aa7ec25ef11a81ab154ffc0695be2aee6
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2016-07-25 22:41:47 +0300

    solved programming-assignment-2 - CoveringSegments

--- programming-assignment-2/src/main/java/CoveringSegments.java
+++ programming-assignment-2/src/main/java/CoveringSegments.java
@@ -1,37 +1,58 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class CoveringSegments {
 
-    private static int[] optimalPoints(Segment[] segments) {
-        //write your code here
-        int[] points = new int[2 * segments.length];
-        for (int i = 0; i < segments.length; i++) {
-            points[2 * i] = segments[i].start;
-            points[2 * i + 1] = segments[i].end;
+    public static List<Integer> optimalPoints(List<Segment> segments) {
+        Collections.sort(segments);
+        int rightMostPoint = -1;
+        List<Integer> points = new ArrayList<>();
+        for (Segment segment : segments) {
+            if (!segment.isInSegment(rightMostPoint)) {
+                rightMostPoint = segment.end;
+                points.add(rightMostPoint);
+            }
         }
         return points;
     }
 
-    private static class Segment {
+    public static class Segment implements Comparable<Segment> {
         int start, end;
 
         Segment(int start, int end) {
             this.start = start;
             this.end = end;
         }
+
+        boolean isInSegment(int point) {
+            return point >= start && point <= end;
+        }
+
+        @Override
+        public int compareTo(Segment s) {
+            if (end > s.end) {
+                return 1;
+            } else if (end < s.end) {
+                return -1;
+            }
+            return 0;
+        }
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        Segment[] segments = new Segment[n];
+        List<Segment> segments = new ArrayList<>(n);
         for (int i = 0; i < n; i++) {
             int start, end;
             start = scanner.nextInt();
             end = scanner.nextInt();
-            segments[i] = new Segment(start, end);
+            segments.add(new Segment(start, end));
         }
-        int[] points = optimalPoints(segments);
-        System.out.println(points.length);
+        List<Integer> points = optimalPoints(segments);
+        System.out.println(points.size());
         for (int point : points) {
             System.out.print(point + " ");
         }
--- programming-assignment-2/src/test/groovy/CoveringSegmentsTest.groovy
+++ programming-assignment-2/src/test/groovy/CoveringSegmentsTest.groovy
@@ -1,9 +1,27 @@
 import org.junit.Rule
 import org.junit.rules.Stopwatch
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import static CoveringSegments.Segment
+import static CoveringSegments.optimalPoints
 
 
 class CoveringSegmentsTest extends Specification {
     @Rule
     public Stopwatch stopwatch = new MeasuringRule()
+
+    @Unroll
+    def "optimalPoints should be #expectedResult"() {
+        expect:
+        optimalPoints(segments) == expectedResult
+        where:
+        segments                             || expectedResult
+        [s(1, 3), s(2, 5), s(3, 6)]          || [3]
+        [s(4, 7), s(1, 3), s(2, 5), s(5, 6)] || [3, 6]
+    }
+
+    def s(start, end) {
+        new Segment(start, end)
+    }
 }
