374c5b6f86b471bc9900ee171b3c5f461839124b
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2016-08-01 13:27:55 +0300

    solved programming-assignment-3 Inversions

--- programming-assignment-3/src/main/java/Inversions.java
+++ programming-assignment-3/src/main/java/Inversions.java
@@ -1,28 +1,47 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Inversions {
-
-    private static long getNumberOfInversions(int[] a, int[] b, int left, int right) {
-        long numberOfInversions = 0;
-        if (right <= left + 1) {
-            return numberOfInversions;
-        }
-        int ave = (left + right) / 2;
-        numberOfInversions += getNumberOfInversions(a, b, left, ave);
-        numberOfInversions += getNumberOfInversions(a, b, ave, right);
-        //write your code here
-        return numberOfInversions;
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] a = new int[n];
+        int[] b = new int[n];
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        int[] b = new int[n];
-        System.out.println(getNumberOfInversions(a, b, 0, a.length));
+        System.out.println(getNumberOfInversions(a, b, 0, a.length - 1));
+    }
+
+    // merge and count
+    private static long merge(int[] a, int[] aux, int lo, int mid, int hi) {
+        long inversions = 0;
+        // copy to aux[]
+        System.arraycopy(a, lo, aux, lo, hi + 1 - lo);
+
+        // merge back to a[]
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > hi) a[k] = aux[i++];
+            else if (aux[j] < aux[i]) {
+                a[k] = aux[j++];
+                inversions += (mid - i + 1);
+            } else a[k] = aux[i++];
+        }
+        return inversions;
+    }
+
+    // return the number of inversions in the subarray b[lo..hi]
+    // side effect b[lo..hi] is rearranged in ascending order
+    public static long getNumberOfInversions(int[] a, int[] b, int lo, int hi) {
+        long inversions = 0;
+        if (hi <= lo) return 0;
+        int mid = lo + (hi - lo) / 2;
+        inversions += getNumberOfInversions(a, b, lo, mid);
+        inversions += getNumberOfInversions(a, b, mid + 1, hi);
+        inversions += merge(a, b, lo, mid, hi);
+        return inversions;
     }
 }
 
--- programming-assignment-3/src/test/groovy/InversionsTest.groovy
+++ programming-assignment-3/src/test/groovy/InversionsTest.groovy
@@ -6,4 +6,13 @@ import spock.lang.Specification
 class InversionsTest extends Specification {
     @Rule
     public Stopwatch stopwatch = new MeasuringRule()
+
+    def "number of inversions in #a should be #expectedResult"() {
+        given:
+        expect:
+        Inversions.getNumberOfInversions(a, new int[a.length], 0, a.length-1) == expectedResult
+        where:
+        a                        || expectedResult
+        [2, 3, 9, 2, 9] as int[] || 2
+    }
 }
